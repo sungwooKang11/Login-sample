ef4be8c8bf6f6159c3e15459069c72bbdd4d0b12
sungwookang <sungwookang11@gmail.com>
2022-03-14 23:19:44 +0900

    ...

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const email = document.querySelector(".email");
 const nickName = document.querySelector(".nickname");
 const passWord = document.querySelector(".password");
+const confirm = document.querySelector(".confirm");
