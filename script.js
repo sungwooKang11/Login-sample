5b121bb7752348987927273e8d44ca30ca447533
sungwookang <sungwookang11@gmail.com>
2022-03-15 15:15:50 +0900

    ...

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const email = document.querySelector(".email");
 const nickName = document.querySelector(".nickname");
 const passWord = document.querySelector(".password");
-const confirm = document.querySelector(".confirm");
+const confirmVal = document.querySelector(".confirm");
 const createBtn = document.querySelector(".createBtn");
 
+
+
 const getValue = () => {
-    console.log(email.value);
-}
+    const info1 = {Email: email.value};
+    const info2 = {id: nickName.value};
+    const info3 = {pw: passWord.value};
+    const info4 = {check: confirmVal.value};
+
+    const account = {
+        ...info1, 
+        ...info2,
+        ...info3,
+        ...info4
+    };
+    console.log(account);
+};
 
 createBtn.addEventListener("click", getValue);
