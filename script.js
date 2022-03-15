c1b4abfef4e1aa7c8481b7731d99363e0a588380
sungwookang <sungwookang11@gmail.com>
2022-03-15 15:21:09 +0900

    ...

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const passWord = document.querySelector(".password");
 const confirmVal = document.querySelector(".confirm");
 const createBtn = document.querySelector(".createBtn");
 
-
+const confirmedPW = "";
 
 const getValue = () => {
     const info1 = {Email: email.value};
     const info2 = {id: nickName.value};
     const info3 = {pw: passWord.value};
-    const info4 = {check: confirmVal.value};
+    const info4 = {check: confirmedPW};
 
     const account = {
         ...info1, 
@@ -20,5 +20,13 @@ const getValue = () => {
     };
     console.log(account);
 };
+
+const confirmPW = () => {
+    if (passWord === confirmVal) {
+        confirmedPW = "true";
+    }else {
+        confirmedPW = "false";
+    }
+}
 
 createBtn.addEventListener("click", getValue);
