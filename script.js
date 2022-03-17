0ffa80f3c055175c8231803b1d7d7ca39638de9c
sungwookang <sungwookang11@gmail.com>
2022-03-17 23:23:49 +0900

    finish

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const nickName = document.querySelector(".nickname"); //아이디
 const passWord = document.querySelector(".password"); //입력 비밀번호
 const confirmVal = document.querySelector(".confirm"); //확인 비밀번호
 const createBtn = document.querySelector(".createBtn"); //계정 생성 버튼
-const spareBtn = document.querySelector(".question"); 
+const spareBtn = document.querySelector(".question");
 
-// let confirmedResult = false; //비밀번호 확인결과값
-
-const getValue = () => { //계정 객체 생성
+const getValue = () => { //계정 객체 생성 
     const info1 = {Email: email.value};
     const info2 = {ID: nickName.value};
     const info3 = {PW: passWord.value};
@@ -21,13 +19,6 @@ const getValue = () => { //계정 객체 생성
         ...info4
     };
 
-    // const obj = {
-    //     "pw" : passWord.value, 
-    //     "cfp" : confirmVal.value,
-    //     "tf":confirmedResult
-    // }
-
-    // function confirmPW() { //비밀번호 확인 처리
     if (passWord.value === confirmVal.value) {
             account.CheckPW = true;
             alert("Successfully created!");
